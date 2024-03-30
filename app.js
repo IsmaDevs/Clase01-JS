8aebd03b49e114b5f0057c9d0e02c8f131db809f
IsmaDevs <chavezpedrito04@gmail.com>
2024-03-30 01:14:01 -0300

    If, Arrays and Loops in JS

---
+++ app.js
@@ -0,0 +1,59 @@
+let nombre;  // Scope Globl
+let nombre2; // Scope Global
+let edad = 19;
+
+
+// -----------------------------
+
+
+
+// if ternario
+
+// edad > 18 ? alert("Es mayor de edad") : null
+// alert(edad > 18, console.log("Es mayor de 18"))
+
+
+
+// if one-line
+
+// if(edad >= 18) console.log("Eres mayor de 18")
+// else 
+// console.log("No eres mayor")
+
+
+
+// Arrays
+
+// let nombresAlumnos = ["Ismael", "Luciano", "Juan"]
+// let meses = ["enero", "febrero", "marzo", "abril"]
+// let mDias = [31,28,31,30]
+
+
+// document.write("Los meses de mi array son "+meses.join(" - "))
+
+
+// For
+
+// For Común:
+
+// for (let i = 0; i < meses.length; i++) 
+// {       
+//    template strings o bartics
+//      console.log()
+// }
+
+// ForOf:
+
+// for (const mes of meses) {
+//    console.log(mes)
+// }
+
+//document.write("Hola")
+
+
+
+
+
+
+
+
